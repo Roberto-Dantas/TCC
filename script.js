0d619d0227c9f9ae0cb7b7aedfe3bbe73dda82cd
Ro005 <84298912+Roberto-Dantas@users.noreply.github.com>
2024-01-05 11:25:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 let data = await fetch('https://docs.google.com/spreadsheets/d/1DuFnwfhUPxx1ZMN7pD96y1d66B8ZdPfSpdU5PzK9E5Q/gviz/tq?tqx=out:json').then(res => res.text()).then(text => JSON.parse(text.substr(47).slice(0, -2)));
 var optionTema = [];
 var f;
-var numero = [];
+var numero = [0];
 if(typeof data === 'object') {
     let num = -1;
     for(var i = 0; i <= data.table.rows.length -1; i++){
@@ -14,47 +14,69 @@ if(typeof data === 'object') {
             result.forEach(element => {
                 if(element){
                     f = true;
-                };
+                }
             });
             if(!f){
+                console.log(data)
+                optionTema.length = data.table.rows.length
                 num++;
-                optionTema[num] = data.table.rows[i].c[0].v;
-            };
+                for(var i = 0; i <= optionTema.length -1; i++){
+                    optionTema[i] = data.table.rows[i].c[0].v;
+        
+                    var opcao = document.createElement('option');
+                    opcao.text = `${optionTema[i]}`;
+                    document.getElementById("selecao_tema").add(opcao);
+                };
+                let temaSelecionado = data.table.rows[numero[0]].c[1].v;
+                
+                let num0 = -1;
+                for(var i = 0; i <= data.table.rows.length -1; i++){
+                    if(data.table.rows[i].c[0].v === temaSelecionado){
+                        num0++;
+                        numero[num0] = i;
+                    }
+                }
+                for(var i = 0; i <= numero.length -1; i++){ 
+                    var opcao = document.createElement('option');
+                    opcao.text = `${data.table.rows[numero[i]].c[1].f}`;
+                    opcao.value = `${numero[i]}`;
+                    document.getElementById("problema").add(opcao);
+                }
+                dados(numero[0]);
+            }
             f = false;
             
         };
-    };
-    for(var i = 0; i <= optionTema.length -1; i++){
-        var opcao = document.createElement('option');
-        opcao.text = `${optionTema[i]}`;
-        document.getElementById("selecao_tema").add(opcao);
-    };
+       
+    }
+    
 }else{
     throw new Error('Sem conexão com a planilha dos dados');
-};
+}
 document.getElementById("selecao_tema").addEventListener('change', function(){
     document.getElementById("problema").innerHTML="";
     numero.length = 0;
     let temaSelecionado = this.options[this.selectedIndex].text;
+
     let num = -1;
     for(var i = 0; i <= data.table.rows.length -1; i++){
         if(data.table.rows[i].c[0].v === temaSelecionado){
             num++;
             numero[num] = i;
-        };
-    };
+        }
+    }
     for(var i = 0; i <= numero.length -1; i++){ 
         var opcao = document.createElement('option');
-        opcao.text = `${data.table.rows[numero[i]].c[1].v}`;
+        opcao.text = `${data.table.rows[numero[i]].c[1].f}`;
         opcao.value = `${numero[i]}`;
         document.getElementById("problema").add(opcao);
-    };
+    }
     dados(numero[0]);
-});
+})
 document.getElementById("problema").addEventListener('change', function(){
     if(numero[1] != undefined){
         dados(parseInt(this.options[this.selectedIndex].value));
-    };
+    }
 });
 function dados(problemaSelecionado){
     document.querySelector("#grupo ul").innerHTML="";
@@ -74,7 +96,7 @@ function dados(problemaSelecionado){
     pon.style.strokeDashoffset = 440 - (440* artigo) /100;
     porcentagem.style.transform = `rotateZ(${artigo * 3.6}deg)`;
 
-    var integrantes = data.table.rows[problemaSelecionado].c[4].v;
+    var integrantes = data.table.rows[problemaSelecionado].c[4].v
     var re = /\s*-\s*/;
     var grupo = integrantes.split(re);
     let liElemento;
@@ -82,7 +104,7 @@ function dados(problemaSelecionado){
         liElemento = document.createElement('li');
         liElemento.textContent = grupo[i];
         document.querySelector("#grupo ul").appendChild(liElemento);
-    };
+    }
     grupo.length = 0;
 
-};
+}
